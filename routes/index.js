0b25a486067e1dbce24bd5cfb30e0bc1d144037e
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-10-02 15:39:07 +0700

    update task

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "San Tech" });
+});
+
+router.get("/profil", function (req, res) {
+  res.render("profil");
+});
+
+module.exports = router;
